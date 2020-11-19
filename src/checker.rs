67a9b38ef266c1e0fb75300c5bb3e039c5544c0d
shivmoha@utexas.edu <utaustin1600>
2020-11-19 18:31:25 +0530

    latest

--- src/checker.rs
+++ src/checker.rs
@@ -6,17 +6,15 @@
 //! are found, and the number of clients, participants. Loads and analyses 
 //! log files to check a handful of correctness invariants. 
 //! 
-extern crate clap;
-extern crate ctrlc;
 extern crate log;
 extern crate stderrlog;
-
+extern crate clap;
+extern crate ctrlc;
 use std::collections::HashMap;
-
-use message;
-use message::MessageType;
-use message::ProtocolMessage;
 use oplog::OpLog;
+use message::ProtocolMessage;
+use message::MessageType;
+use message;
 
 ///
 /// check_participant()
@@ -37,18 +35,19 @@ fn check_participant(
     ncommit: usize,
     nabort: usize,
     ccommitted: &HashMap<i32, ProtocolMessage>,
-    plog: &HashMap<i32, ProtocolMessage>,
+    plog: &HashMap<i32, ProtocolMessage>
 ) -> bool {
+
     let mut result = true;
     let pcommitted = plog.iter()
         .filter(|e| (*e.1).mtype == MessageType::CoordinatorCommit)
-        .map(|(k, v)| (k.clone(), v.clone()));
+        .map(|(k,v)| (k.clone(), v.clone()));
     let plcommitted = plog.iter()
         .filter(|e| (*e.1).mtype == MessageType::ParticipantVoteCommit)
-        .map(|(k, v)| (k.clone(), v.clone()));
+        .map(|(k,v)| (k.clone(), v.clone()));
     let paborted = plog.iter()
         .filter(|e| (*e.1).mtype == MessageType::CoordinatorAbort)
-        .map(|(k, v)| (k.clone(), v.clone()));
+        .map(|(k,v)| (k.clone(), v.clone()));
 
     let mcommit: HashMap<i32, message::ProtocolMessage> = pcommitted.collect();
     let mlcommit: HashMap<i32, message::ProtocolMessage> = plcommitted.collect();
@@ -110,6 +109,7 @@ pub fn check_last_run(
     n_requests: i32,
     n_participants: i32,
     logpathbase: &String) {
+
     info!("Checking 2PC run:  {} requests * {} clients, {} participants",
           n_requests,
           n_clients,
@@ -129,19 +129,18 @@ pub fn check_last_run(
     let cmap = lck.lock().unwrap();
     let committed: HashMap<i32, message::ProtocolMessage> =
         cmap.iter().filter(|e| (*e.1).mtype == MessageType::CoordinatorCommit)
-            .map(|(k, v)| (k.clone(), v.clone()))
+            .map(|(k,v)| (k.clone(), v.clone()))
             .collect();
     let aborted: HashMap<i32, message::ProtocolMessage> =
         cmap.iter().filter(|e| (*e.1).mtype == MessageType::CoordinatorAbort)
-            .map(|(k, v)| (k.clone(), v.clone()))
+            .map(|(k,v)| (k.clone(), v.clone()))
             .collect();
     let ncommit = committed.len();
     let nabort = aborted.len();
 
-    for (p, v) in logs.iter() {
+    for(p, v) in logs.iter() {
         let plck = v.arc();
         let plog = plck.lock().unwrap();
         check_participant(p, ncommit, nabort, &committed, &plog);
     }
 }
-
